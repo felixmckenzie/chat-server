99c69d843ff7f060ce08230a02e83955d0162229
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-02-21 09:12:23 +1100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
+import {ApolloServerPluginDrainHttpServer} from '@apollo/server/plugin/drainHttpServer'
+import {json} from 'body-parser'
+import cors from 'cors'
+import http from 'http'
+
+
+const app = express()
+const httpServer = http.createServer(app)
+
+async function createApolloServer(){
+const server = new ApolloServer({
+typeDefs,
+resolvers,
+plugins: [ApolloServerPluginDrainHttpServer({httpServer})]
+})
+
+await server.start()
+
+return server
+}
+
+const apolloServer = createApolloServer()
+
+app.use('/graphql', cors<cors.CorsRequest>(), json(), expressMiddleware(apolloServer) )
+
+app.listen({port: PORT}, ()=> {
+console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`)
+})
