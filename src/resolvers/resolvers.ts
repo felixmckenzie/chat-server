3dd4f215ca31f580aba921285e94ed2c77622378
Felix Mckenzie <felixmckenzie90@gmail.com>
2023-04-04 14:36:56 +1000

    add createChannel resolver

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -1,74 +1,99 @@
-import {Context} from '../context'
-import { UserInput, UserRegisterInput } from '../types/resolvers-types'
+import { Context } from '../context'
+import { CreateChannelInput, UserInput, UserRegisterInput, Channel } from '../types/resolvers-types'
 import { auth0ManagemenClient } from '../utils/auth0.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 export const resolvers = {
-    Query: {
-        getUser: (_parent, args: {id: number}, context: Context ) => {
-            return context.prisma.user.findUnique({
-                where: {id: args.id || undefined}
-            })
-        },
-        getChat: (_parent, args: {id: number}, context: Context) => {
-            return context.prisma.chat.findUnique({
-                where: {id: args.id || undefined}
-            })
-        },
-        getAllUserChats: (_parent, args: {userInput: UserInput}, context: Context) => {
-            return context.prisma.user.findUnique({
-                where: {id: args.userInput.id || undefined, email: args.userInput.email}
-            }).chats()
-        },
-        hello: () => 'Hello, world!',
+  Query: {
+    getUser: (_parent, args: { id: number }, context: Context) => {
+      return context.prisma.user.findUnique({
+        where: { id: args.id || undefined },
+      })
     },
-    Mutation: {
-        createUser: async (_parent, args: {input: UserRegisterInput},context: Context) => {
-           
-            const hashedPassword = await bcrypt.hash(args.input.password, 10)
-    
-            const auth0User = await auth0ManagemenClient.createUser({
-                connection: 'Username-Password-Authentication',
-                email: args.input.email,
-                password: hashedPassword,
-                user_metadata:{
-                    givenName: args.input.givenName,
-                    familyName: args.input.familyName,
-                }
-            })
+    getChat: (_parent, args: { id: number }, context: Context) => {
+      return context.prisma.channel.findUnique({
+        where: { id: args.id || undefined },
+      })
+    },
+    getAllUserChannels: (_parent, args: { userInput: UserInput }, context: Context) => {
+      return context.prisma.user
+        .findUnique({
+          where: { id: args.userInput.id || undefined, email: args.userInput.email },
+        })
+        .Channels()
+    },
+    hello: () => 'Hello, world!',
+  },
+  Mutation: {
+    createUser: async (_parent, args: { input: UserRegisterInput }, context: Context) => {
+      const hashedPassword = await bcrypt.hash(args.input.password, 10)
 
-            let user
-        try {
-            user = await context.prisma.user.create({
-                 data: {
-                    givenName: args.input.givenName,
-                    familyName: args.input.familyName,
-                    mobilePhone: args.input.mobilePhone,
-                     email: args.input.email,
-                     password: hashedPassword,
-                    isActive: args.input.isActive,
-                    avatar: args.input.avatar,
-                    role: args.input.role,
-                },
-            })
-        } catch (error) {
+      const auth0User = await auth0ManagemenClient.createUser({
+        connection: 'Username-Password-Authentication',
+        email: args.input.email,
+        password: hashedPassword,
+        user_metadata: {
+          givenName: args.input.givenName,
+          familyName: args.input.familyName,
+        },
+      })
+
+      let user
+      try {
+        user = await context.prisma.user.create({
+          data: {
+            givenName: args.input.givenName,
+            familyName: args.input.familyName,
+            mobilePhone: args.input.mobilePhone,
+            email: args.input.email,
+            password: hashedPassword,
+            isActive: args.input.isActive,
+            avatar: args.input.avatar,
+            role: args.input.role,
+          },
+        })
+      } catch (error) {
         console.error('Error creating user with Prisma:', error)
-        }
-            const token = jwt.sign( {
-                                userId: user.id,
-                                email: user.email,
-                                role: user.role,
-                            },
-                            process.env.JWT_SECRET,
-                            { expiresIn: '1h' })
-            
-          
-             return {user: user, token: token}
-        }
+      }
+      const token = jwt.sign(
+        {
+          userId: user.id,
+          email: user.email,
+          role: user.role,
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+      )
 
+      return { user: user, token: token }
     },
-    Subscription: {
+    createChannel: async (_parent, args: { input: CreateChannelInput }, context: Context) => {
+      const { name, userIds } = args.input
 
+      const users = await context.prisma.user.findMany({
+        where: {
+          id: {
+            in: userIds,
+          },
+        },
+      })
+      if (users.some((user) => !user)) {
+        throw new Error('One or more users not found')
+      }
+
+      const channel = await context.prisma.channel.create({
+        data: {
+          name: name,
+          members: {
+            connect: users.map((user) => ({ id: user.id })),
+          },
+          isGroupChat: true,
+        },
+      })  
+
+      return channel
     },
+  },
+  Subscription: {},
 }
